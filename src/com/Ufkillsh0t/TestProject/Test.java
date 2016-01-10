7d3db3e178e4e53dfe80a99d4965bf9130dd81a7
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2016-01-10 23:46:27 +0100

    Test java git project

---
+++ src/com/Ufkillsh0t/TestProject/Test.java
@@ -0,0 +1,11 @@
+package com.Ufkillsh0t.TestProject;
+
+public class Test {
+	
+	private static String test = "Test";
+	
+	public static void main(String [] args)
+	{
+		System.out.println(test);
+	}
+}
